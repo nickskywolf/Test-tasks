21ab70bfaf6628f6511d9f3328fdca036e5add50
nickskywolf <nickskywolf@gmail.com>
2023-08-14 19:37:39 +0300

    Update dz10_v1.py

--- dz10/dz10_v1.py
+++ dz10/dz10_v1.py
@@ -1,6 +1,7 @@
 import re
 from collections import UserDict
 
+# Базовий клас для полів
 class Field:
     def __init__(self, value=None):
         self.value = value
@@ -8,6 +9,7 @@ class Field:
     def __str__(self):
         return str(self.value)
 
+# Клас для імені з валідацією
 class Name(Field):
     def __init__(self, value):
         super().__init__(value)
@@ -19,6 +21,7 @@ class Name(Field):
         pattern = r"^[a-zA-Z']{2,}$"
         return re.match(pattern, name) is not None
 
+# Клас для телефону з валідацією
 class Phone(Field):
     def __init__(self, value):
         super().__init__(value)
@@ -31,12 +34,14 @@ class Phone(Field):
         pattern = r"^\d{10}$"
         return re.match(pattern, phone) is not None
 
+# Клас для запису
 class Record:
-    def __init__(self, name_value):
-        self.name = Name(name_value)
-        self.phones = []
+    def __init__(self, name, phone):
+        self.name = name
+        self.phones = [phone]
 
     def add_phone(self, phone_value):
+        # Створюємо об'єкт телефону і передаємо значення в конструктор
         phone = Phone(phone_value)
         self.phones.append(phone)
 
@@ -46,21 +51,23 @@ class Record:
 
     def edit_phone(self, phone_index, new_phone_value):
         if 0 <= phone_index < len(self.phones):
+            # Змінюємо значення телефону через об'єкт телефону
             self.phones[phone_index].value = new_phone_value
 
     def __str__(self):
         phones_str = ", ".join(str(phone) for phone in self.phones)
         return f"Name: {self.name}\nPhones: {phones_str}"
 
+# Клас для адресної книги
 class AddressBook(UserDict):
     def add_record(self, record):
+        # Додаємо запис до словника, де ключ - це ім'я з об'єкта запису
         self.data[record.name.value] = record
 
 if __name__ == "__main__":
     name = Name('Bill')
     phone = Phone('1234567890')
-    rec = Record(name.value)
-    rec.add_phone(phone.value)
+    rec = Record(name, phone)  # Передаємо об'єкти Name та Phone
     ab = AddressBook()
     ab.add_record(rec)
     
